f0a94c92075c7d1a7936bc1a82d10ea76f4885dd
Rogelio Ventura <rogeliovr94@gmail.com>
2017-09-27 13:20:19 -0500

    Correcion de tabla sql

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -5,9 +5,7 @@
    	$link=db_Connection();
 
 	 $verdadero = $_POST["veracidad"];
-   $paciente = rand(1, 259);
-   $latitud = rand(19340, 19550);
-   $longitud = rand(-99260, -99036);
+   
    $latitud = $latitud/1000;
    $longitud = $longitud/1000;
    $prob_fix = rand(0,1);
@@ -29,13 +27,16 @@
 
 
    for ($n = 0; $n<10; $n++){
+      $paciente = rand(1, 259);
+      $latitud = rand(19340, 19550);
+      $longitud = rand(-99260, -99036);
       $prob_fix = rand(0,1);
       $fix = $arr_fix($prob_fix);
 
       $peticion="INSERT INTO big_data VALUES (NULL, '" .$paciente."', NULL, $latitud, $longitud, '" .$fix."', $verdadero)"; 
       
       $link->query($peticion);
-       
+      echo $peticion; 
     }
 
     

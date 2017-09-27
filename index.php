968a1b434750cb8a1c7d4ca459bb7f044a76bad7
Rogelio Ventura <rogeliovr94@gmail.com>
2017-09-27 13:44:14 -0500

    Visualización de datos

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include "../../connect_e_health.php";	
+	
+	$link= db_Connection();
+	$result= $link->query("SELECT * FROM big_data ORDER BY `ID` DESC");
+?>
+
+<html>
+   <head>
+      <title>Info Resultados</title>
+      <link rel="stylesheet" href="../../../bootstrap/css/bootstrap.min.css">
+   </head>
+<body>
+   <div class="container">
+	    <img src="../../../images/logo.png">
+	    </br></br>
+   
+   <h1>Información de Resultados</h1>
+   <a href="boton_resultados.php">Descarga de Base de Datos</a>
+   <div class="table-responsive">
+	   <table class="table table-striped">
+			<tr>
+				<td>&nbsp;ID&nbsp;</td>
+				<td>&nbsp;Paciente&nbsp;</td>
+				<td>&nbsp;Fecha&nbsp;</td>
+				<td>&nbsp;Latitud&nbsp;</td>
+				<td>&nbsp;Longitud&nbsp;</td>
+				<td>&nbsp;Fix&nbsp;</td>
+				<td>&nbsp;Verdadero/Falso&nbsp;</td>
+				
+			</tr>
+
+	      <?php 
+			  if($result!==FALSE){
+			     while($row = $result->fetch_assoc()) {
+			        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></td><td> &nbsp;%s&nbsp; </td></tr>", 
+			           $row["ID"], $row["Paciente"], $row["Fecha"], $row["Latitud"], $row["Longitud"], $row["Fix"], $row["Verdadero"]);
+			     }
+			     $link->close();
+
+			  }
+	      ?>
+	   
+	   </table>	
+   </div>
+   
+  </div>
+</body>
+</html>
+

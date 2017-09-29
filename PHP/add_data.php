c9cbe1461ac60c21fbee71ffc864fa5bf3f79546
Rogelio Ventura <rogeliovr94@gmail.com>
2017-09-28 23:55:34 -0500

    Manejo de Base de datos

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -7,7 +7,7 @@
   $arr_fix = array("GPS", "Celular");
 
 
-  for ($n = 1; $n<17; $n++){
+  for ($n = 1; $n<12; $n++){
     $paciente = rand(1, 259);
     $latitud = rand(19340, 19550);
     $longitud = rand(-99260, -99036);
@@ -50,7 +50,7 @@
 
     $link->query($peticion_update);
 
-    $peticion_insert="INSERT INTO big_data VALUES (NULL, '" .$paciente."', NULL, NULL, $longitud, '" .$fix."', $verdadero)"; 
+    $peticion_insert="INSERT INTO big_data VALUES (NULL, '" .$paciente."', NULL, $latitud, NULL, '" .$fix."', $verdadero)"; 
     
     $link->query($peticion_insert);
     echo $peticion; 

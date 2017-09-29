be0731e4a8570d48cd08ea6427bb2afa4ebab593
Rogelio Ventura <rogeliovr94@gmail.com>
2017-09-28 23:50:05 -0500

    Manejo de Base de datos

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -7,7 +7,7 @@
   $arr_fix = array("GPS", "Celular");
 
 
-  for ($n = 0; $n<2; $n++){
+  for ($n = 0; $n<1032; $n++){
     $paciente = rand(1, 259);
     $latitud = rand(19340, 19550);
     $longitud = rand(-99260, -99036);

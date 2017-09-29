ef22e6bf6692380351ffce5ed458e83f34eefa7d
Rogelio Ventura <rogeliovr94@gmail.com>
2017-09-28 22:47:25 -0500

    Upload Data

--- PHP/add_data.php
+++ PHP/add_data.php
@@ -1,6 +1,11 @@
 <?php
   session_start();
   include "../../../connect_e_health.php";	
+  
+  $link=db_Connection();
+	$verdadero = $_POST["veracidad"]; 
+  $arr_fix = array("GPS", "Celular");
+
 
   for ($n = 0; $n<2; $n++){
     $paciente = rand(1, 259);
